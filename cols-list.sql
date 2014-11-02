09eb8a29536312850b4b198f0a92fa62d3d5eeab
stef990 <stef990@users.noreply.github.com>
2014-11-02 02:49:12 +0000

    Update cols-list.sql

--- cols-list.sql
+++ cols-list.sql
@@ -10,6 +10,6 @@ values("821/7355","Dolphin","Blue");
 insert into towels (color,code,name)
 values("Lilac","830/1921","Daisy");
 insert into towels (code,name)
-values("8230/7370","Star");
+values("823/7370","Star");
 select * from towels;
 
