3ec67ce47aebf51c4fa44918123dae3124bd14e5
stef990 <stef990@users.noreply.github.com>
2014-11-02 02:45:23 +0000

    Create cols-list.sql

---
+++ cols-list.sql
@@ -0,0 +1,13 @@
+use my_database;
+create table towels
+(
+code varchar(8) not null primary key,
+name varchar(20) not full,
+color varchar(20) default "White"
+);
+insert into towels (code,name,color)
+values("821/7355,"Dolphin","Blue");
+insert into towels (color,code,name)
+values("Lilac","830/1921","Daisy");
+insert into towels (code,name)
+values("8230/7370,"Star");
