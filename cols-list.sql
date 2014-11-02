77e173d1a4e221d580b163c6cbbd41be75c7407f
stef990 <stef990@users.noreply.github.com>
2014-11-02 02:46:35 +0000

    Update cols-list.sql

--- cols-list.sql
+++ cols-list.sql
@@ -6,8 +6,8 @@ name varchar(20) not full,
 color varchar(20) default "White"
 );
 insert into towels (code,name,color)
-values("821/7355,"Dolphin","Blue");
+values("821/7355","Dolphin","Blue");
 insert into towels (color,code,name)
 values("Lilac","830/1921","Daisy");
 insert into towels (code,name)
-values("8230/7370,"Star");
+values("8230/7370","Star");
